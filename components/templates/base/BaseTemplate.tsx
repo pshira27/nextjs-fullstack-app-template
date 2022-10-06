f6e307d41ce57609af04efbe7b67b9b6745bf2c4
Shira-P <pisit.sr40@gmail.com>
2022-10-06 09:54:12 +0700

    feat: add BaseTemplate component

---
+++ components/templates/base/BaseTemplate.tsx
@@ -0,0 +1,11 @@
+import style from './BaseTemplate.module.css';
+
+export interface IBaseTemplate {
+  sampleTextProp: string;
+}
+
+const BaseTemplate: React.FC<IBaseTemplate> = ({ sampleTextProp }) => {
+  return <div className={style.container}>{sampleTextProp}</div>;
+};
+
+export default BaseTemplate;
